77f3b1448de87f0f6433cbdcf04db87dacd53f6f
Ravinjith <ravinjith2001rv@gmail.com>
2025-04-27 11:10:28 +0530

    commit-10-model-ranidu-annotation

---
+++ src/main/java/com/skillshare/model/User.java
@@ -0,0 +1,19 @@
+package com.skillshare.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.index.Indexed;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
