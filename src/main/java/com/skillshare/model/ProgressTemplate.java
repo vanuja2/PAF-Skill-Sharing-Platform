81fa7adf0ab6fa16f3c9d06fedf4e2643c101a1c
vanuja2 <vanujaalagiyawanna@gmail.com>
2025-04-27 11:10:29 +0530

    commit-15-Vanuja-addDataFields

--- src/main/java/com/skillshare/model/ProgressTemplate.java
+++ src/main/java/com/skillshare/model/ProgressTemplate.java
@@ -1,5 +1,24 @@
 package com.skillshare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.util.List;
+import java.util.ArrayList;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProgressTemplate {
+    private String type;
     
+    @Builder.Default
+    private List<String> completed = new ArrayList<>();
+    
+    @Field("skills_learned")
+    @Builder.Default
+    private List<String> skillsLearned = new ArrayList<>();
 }
