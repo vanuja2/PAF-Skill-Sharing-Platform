2cf1da556f5f6c732b9e6d30ccdc30ff4653ab80
vanuja2 <vanujaalagiyawanna@gmail.com>
2025-04-27 10:35:29 +0530

    commit-6-Vanuja-updategetmapping-PostController

--- src/main/java/com/skillshare/controller/PostController.java
+++ src/main/java/com/skillshare/controller/PostController.java
@@ -1,5 +1,32 @@
 package com.skillshare.controller;
 
+import com.skillshare.model.Post;
+import com.skillshare.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import java.time.Instant;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
     
-}
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        try {
+            log.debug("Fetching all posts");
+            List<Post> posts = postRepository.findAll();
+            return ResponseEntity.ok(posts);
+        } catch (Exception e) {
+            log.error("Error fetching posts", e);
+            throw new RuntimeException("Failed to fetch posts", e);
+        }
+    }
