f4f1689a66632c3244b25d373536d503ae58998e
Ravinjith <ravinjith2001rv@gmail.com>
2025-04-27 11:11:34 +0530

    commit-11-model-ranidu-assign

--- src/main/java/com/skillshare/model/User.java
+++ src/main/java/com/skillshare/model/User.java
@@ -17,3 +17,41 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    
+    @Indexed(unique = true)
+    private String email;
+    
+    private String password;
+    
+    @Field("first_name")
+    private String firstName;
+    
+    @Field("last_name")
+    private String lastName;
+    
+    private String address;
+    
+    private String birthday;
+    
+    @Field("avatar_url")
+    private String avatarUrl;
+    
+    private String bio;
+    
+    @Builder.Default
+    @Field("following_ids")
+    private List<String> followingIds = new ArrayList<>();
+    
+    @Builder.Default
+    @Field("follower_ids")
+    private List<String> followerIds = new ArrayList<>();
+    
+    @Field("created_at")
+    private Instant createdAt;
+    
+    @Field("updated_at")
+    private Instant updatedAt;
+}
