3e435f40be01573487df628ff5d91d6f7b1ffe3e
wathmiwarshana <IT22137296@my.sliit.lk>
2025-04-27 11:12:01 -0100

    commit-14-wathmi-update notification controller

--- src/main/java/com/skillshare/controller/NotificationController.java
+++ src/main/java/com/skillshare/controller/NotificationController.java
@@ -1,5 +1,20 @@
 package com.skillshare.controller;
 
+import com.skillshare.model.Notification;
+import com.skillshare.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
 public class NotificationController {
     
 }
