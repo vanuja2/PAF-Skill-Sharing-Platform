ac4449dc6682ac927fa2ef9168d4f2667d6455ea
vanuja2 <vanujaalagiyawanna@gmail.com>
2025-04-27 11:05:12 +0530

    commit-13-Vanuja-addAttributes

--- src/main/java/com/skillshare/model/ProjectDetails.java
+++ src/main/java/com/skillshare/model/ProjectDetails.java
@@ -1,5 +1,14 @@
 package com.skillshare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDetails {
-    
-}
+    private String name;
+    private String description;
