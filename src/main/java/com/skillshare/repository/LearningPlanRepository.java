b28e6714977582cc89c3597fbb39b28753bb7d9d
Alokakumari12 <alokakumari1102@gmail.com>
2025-04-27 11:04:51 +0530

    commit-13-Aloka-Repository

---
+++ src/main/java/com/skillshare/repository/LearningPlanRepository.java
@@ -0,0 +1,15 @@
+package com.skillshare.repository;
+
+import com.skillshare.model.LearningPlan;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
+    List<LearningPlan> findByUserId(String userId);
+    List<LearningPlan> findBySkill(String skill);
+    List<LearningPlan> findBySkillLevel(String skillLevel);
+}
+
+public class LearningPlanRepository {
+    
+}
