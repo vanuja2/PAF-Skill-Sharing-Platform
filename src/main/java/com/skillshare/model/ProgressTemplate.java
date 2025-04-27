d5fb3f279c632948e475bb5dc882115038e75041
Alokakumari12 <alokakumari1102@gmail.com>
2025-04-27 11:33:06 +0530

    commit-20-Aloka-ProjectDetails1

---
+++ src/main/java/com/skillshare/model/ProgressTemplate.java
@@ -0,0 +1,15 @@
+package com.skillshare.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProgressTemplate {
+    
+}
