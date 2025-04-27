821bf812b5103380db621527ae8b08fae44a6d12
vanuja2 <vanujaalagiyawanna@gmail.com>
2025-04-27 10:50:59 +0530

    commit-9-Vanuja-createMediaItem.java

--- src/main/java/com/skillshare/model/MediaItem.java
+++ src/main/java/com/skillshare/model/MediaItem.java
@@ -1,5 +1,22 @@
 package com.skillshare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MediaItem {
+    private String id;
+    private String filename;
+    private String contentType;
+    private String description;
+   
     
+    @Transient
+    private byte[] data;
 }
