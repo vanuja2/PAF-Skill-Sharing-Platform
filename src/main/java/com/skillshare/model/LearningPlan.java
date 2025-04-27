30bf30cad62a7ae14c2a339e426eb0b56ebfcbe8
Alokakumari12 <alokakumari1102@gmail.com>
2025-04-27 10:49:39 +0530

    commit-8-aloka-model-lerningPlans

---
+++ src/main/java/com/skillshare/model/LearningPlan.java
@@ -0,0 +1,26 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "learning_plans")
+public class LearningPlan {
+    @Id
+    private String id;
+    
+    @Field("user_id")
+    private String userId;
+    
+    private String title;
+    private String thumbnail;
+    private String skill;
