1ce39b62fb8e141787d79a0ba19ab2676cef85d1
vanuja2 <vanujaalagiyawanna@gmail.com>
2025-05-07 10:47:34 +0530

    commit-20-vanuja-create-AuthService.java

--- src/main/java/com/skillshare/service/AuthService.java
+++ src/main/java/com/skillshare/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.skillshare.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     
 }
